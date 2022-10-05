39b2b2d1bd487fe3f26830406b12d1979865e537
k164596848 <k164596848@gmail.com>
2022-10-05 13:36:57 +0800

    first

---
+++ authServer.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+
+// call "express" library 
+const express = require('express')
+const app = express()
+
+const jwt =require('jsonwebtoken')
+
+app.use(express.json())
+
+// usually we wiil store the refreshToken in a Database table or Redis cache
+let refreshTokens =[]
+
+app.post('/token',(req,res)=>{
+// first valid the refreshToken then create a new accessToken, 
+// send a respose whit new accessToken to client
+
+    const refreshToken = req.body.token
+
+    //if the refreshToken is already known then send 401
+    if(refreshToken == null ) return res.sendStatus(401)
+
+    // if the refreshToken is not exsist then send 403
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+
+    jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err, user)=>{
+
+        if(err) return res.sendStatus(403)
+
+        const accessToken = generateAccessToken({ name:user.name})
+        // response  the new accessToken
+        res.json({accessToken:accessToken})
+    })
+
+})
+
+
+app.post('/login',(req,res)=>{
+    //Authenticate user 
+
+    const username = req.body.username
+    const user = {name: username}
+
+    console.log(user)
+    // call the generateAccessToken to create a secret key 
+    const accessToken = generateAccessToken(user)
+    // use the 
+    const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+    // 
+    refreshTokens.push(refreshToken)
+
+    res.json({accessToken: accessToken , refreshToken: refreshToken})
+})
+
+function generateAccessToken(user){
+    // add the expiration date in the jwt access token 
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '30s'})
+
+}
+
+app.listen(4000)
---
+++ server.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+const jwt =require('jsonwebtoken')
+
+app.use(express.json())
+
+const posts = [
+    {
+        username:'Danny',
+        title:'Staff Engineer',
+        role:'GHP Mentor'
+    },
+    {
+        username:'Jim',
+        title:'Engineer',
+        role:'GHP New Commer'
+
+    }
+]
+
+app.get('/posts',authenticateToken, (req,res) =>{
+    res.json(posts.filter(post => post.username === req.user.name))
+}) 
+
+
+
+app.post('/login',(req,res)=>{
+    //Authenticate user 
+
+    const username = req.body.username
+    const user = {name: username}
+
+    console.log(user)
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken})
+})
+
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    // make sure there is a token in authHeader
+    if (token == null) {
+        // 401 Unathorized 未授權
+        return res.sendStatus(401)
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+        console.log(err)
+        // 403 Forbidden 無訪問權限
+        if (err) return res.sendStatus(403)
+        req.user = user
+
+        next()
+    })
+    
+}
+
+app.listen(3000)
